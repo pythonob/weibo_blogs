866bae2ff90553cb4bcf08ef8c7c810c5201d327
pythonob <43611604+pythonob@users.noreply.github.com>
2019-04-29 11:09:02 +0800

    Update settings.py

--- weibo_user/settings.py
+++ weibo_user/settings.py
@@ -30,7 +30,7 @@ ITEM_PIPELINES = {
     'weibo_user.pipelines.WeiboUserPipeline': 543,
     #'scrapy_redis.pipelines.RedisPipeline': 300,
 }
-CLOSESPIDER_ITEMCOUNT = 120000 # 抓取10万条记录
+CLOSESPIDER_ITEMCOUNT = 120000 # 抓取12万条记录
 
 USER_AGENTS = [
         "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; "
